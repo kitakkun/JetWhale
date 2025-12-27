4a616e197c4f84df2483cbcf3636c16dbfd260cb
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-12-27 23:32:59 +0900

    chore: set up gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "JetWhale"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
