f2a17e7aa226d3ed330f769a3cd9c94a1c463d75
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-12-31 21:16:00 +0900

    refactor: simplify context receiver type reference

--- jetwhale-debugger-host/feature/settings/src/main/kotlin/com/kitakkun/jetwhale/host/settings/plugin/PluginSettingsScreenPresenter.kt
+++ jetwhale-debugger-host/feature/settings/src/main/kotlin/com/kitakkun/jetwhale/host/settings/plugin/PluginSettingsScreenPresenter.kt
@@ -4,12 +4,13 @@ import androidx.compose.runtime.Composable
 import com.kitakkun.jetwhale.host.architecture.EventEffect
 import com.kitakkun.jetwhale.host.architecture.EventFlow
 import com.kitakkun.jetwhale.host.model.PluginMetaData
+import com.kitakkun.jetwhale.host.settings.SettingsScreenContext
 import com.kitakkun.jetwhale.host.settings.component.PluginInfoUiState
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toPersistentList
 import soil.query.compose.rememberMutation
 
-context(screenContext: com.kitakkun.jetwhale.host.settings.SettingsScreenContext)
+context(screenContext: SettingsScreenContext)
 @Composable
 fun pluginSettingsScreenPresenter(
     eventFlow: EventFlow<PluginSettingsScreenEvent>,
