d6d818f7fa85cfebba80f5206449d1b36cb68ce4
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-12-31 13:03:11 +0900

    refactor: move builder functions to a separate file

--- jetwhale-debugger-host-sdk/src/main/kotlin/com/kitakkun/jetwhale/debugger/host/sdk/JetWhaleHostPlugin.kt
+++ jetwhale-debugger-host-sdk/src/main/kotlin/com/kitakkun/jetwhale/debugger/host/sdk/JetWhaleHostPlugin.kt
@@ -1,9 +1,6 @@
-@file:OptIn(InternalJetWhaleHostApi::class)
-
 package com.kitakkun.jetwhale.debugger.host.sdk
 
 import androidx.compose.runtime.Composable
-import kotlinx.serialization.serializer
 
 /**
  * Plugin interface for JetWhale.
@@ -31,54 +28,4 @@ public interface JetWhaleHostPlugin {
      */
     public fun onDispose() {
     }
-}
-
-public inline fun <reified Event, reified Method, reified MethodResult> buildJetWhaleHostPlugin(
-    onReceiveEvent: EventReceiver<Event>,
-    content: PluginUIBuilder<Method, MethodResult>,
-): JetWhaleHostPlugin {
-    return object : JetWhaleHostPlugin {
-        override suspend fun onReceive(context: JetWhaleEventReceiverContext) {
-            onReceiveEvent.receive(context.getDeserializedPayload(serializer()))
-        }
-
-        @Composable
-        override fun Content(context: JetWhaleContentUIBuilderContext) {
-            content.Content {
-                context.dispatch(
-                    methodSerializer = serializer(),
-                    methodResultSerializer = serializer(),
-                    value = it
-                )
-            }
-        }
-    }
-}
-
-public fun buildJetWhaleHostPlugin(
-    content: @Composable () -> Unit,
-): JetWhaleHostPlugin = buildJetWhaleHostPlugin<Unit, Unit, Unit>(
-    onReceiveEvent = { },
-    content = { content() }
-)
-
-public fun buildJetWhalePrimitiveHostPlugin(
-    onReceiveEvent: EventReceiver<String>,
-    content: PluginUIBuilder<String, String>,
-): JetWhaleHostPlugin = buildJetWhaleHostPlugin(
-    onReceiveEvent = onReceiveEvent,
-    content = content,
-)
-
-public fun interface EventReceiver<Event> {
-    public suspend fun receive(event: Event)
-}
-
-public fun interface PluginUIBuilder<Method, MethodResult> {
-    @Composable
-    public fun Content(methodDispatcher: MethodDispatcher<Method, MethodResult>)
-}
-
-public fun interface MethodDispatcher<Method, MethodResult> {
-    public suspend fun dispatch(method: Method): MethodResult?
 }
