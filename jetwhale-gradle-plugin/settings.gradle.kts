fd29c8b9c8b4900c123e6179d2613a365861cb7e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-01-04 11:21:52 +0900

    chore: configure jetwhale-gradle-plugin Gradle project

---
+++ jetwhale-gradle-plugin/settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "jetwhale-gradle-plugin"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
