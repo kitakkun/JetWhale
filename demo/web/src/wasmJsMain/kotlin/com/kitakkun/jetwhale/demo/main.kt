7fd6ac0c0b89e8ff52613c2dcacd658b4ef44046
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-01-24 03:55:51 +0900

    feat: initialize jetwhale agent on web demo app

--- demo/web/src/wasmJsMain/kotlin/com/kitakkun/jetwhale/demo/main.kt
+++ demo/web/src/wasmJsMain/kotlin/com/kitakkun/jetwhale/demo/main.kt
@@ -3,10 +3,13 @@ package com.kitakkun.jetwhale.demo
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.kitakkun.jetwhale.demo.shared.App
+import com.kitakkun.jetwhale.demo.shared.initializeJetWhale
 import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initializeJetWhale()
+
     val body = document.body ?: return
     ComposeViewport(body) {
         App()
