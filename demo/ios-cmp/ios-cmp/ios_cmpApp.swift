c082720cdec2bd0b7de2589f0de6af0223486b2a
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-01-24 14:03:22 +0900

    feat(demo): init jetwhale service to enable debugging features

--- demo/ios-cmp/ios-cmp/ios_cmpApp.swift
+++ demo/ios-cmp/ios-cmp/ios_cmpApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import shared
 
 @main
 struct ios_cmpApp: App {
+    init() {
+        InitializeJetWhaleKt.initializeJetWhale()
+    }
+
     var body: some Scene {
         WindowGroup {
             CMPAppViewControllerWrapper()
