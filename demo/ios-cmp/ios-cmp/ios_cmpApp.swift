7413c17b1bf654d91bc214c7ba0a7f1fa295a578
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-01-24 13:34:54 +0900

    feat: add ios-cmp demo project

---
+++ demo/ios-cmp/ios-cmp/ios_cmpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cmpApp.swift
+//  ios-cmp
+//
+//  Created by kitakkun on 2026/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_cmpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
