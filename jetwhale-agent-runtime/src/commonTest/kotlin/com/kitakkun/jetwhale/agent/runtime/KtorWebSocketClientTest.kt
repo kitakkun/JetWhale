92452ec3290a656318b57a6f322fe00c8330898c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-02-11 15:37:13 +0900

    fix(test): KtorWebSocketClientTest

--- jetwhale-agent-runtime/src/commonTest/kotlin/com/kitakkun/jetwhale/agent/runtime/KtorWebSocketClientTest.kt
+++ jetwhale-agent-runtime/src/commonTest/kotlin/com/kitakkun/jetwhale/agent/runtime/KtorWebSocketClientTest.kt
@@ -1,6 +1,8 @@
 package com.kitakkun.jetwhale.agent.runtime
 
 import com.kitakkun.jetwhale.protocol.InternalJetWhaleApi
+import com.kitakkun.jetwhale.protocol.core.JetWhaleDebuggeeEvent
+import com.kitakkun.jetwhale.protocol.core.JetWhaleDebuggerEvent
 import com.kitakkun.jetwhale.protocol.serialization.JetWhaleJson
 import com.kitakkun.test.annotations.IgnoreNative
 import com.kitakkun.test.annotations.IgnoreWeb
@@ -10,8 +12,8 @@ import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 import io.ktor.server.websocket.WebSocketServerSession
 import io.ktor.server.websocket.WebSockets
+import io.ktor.server.websocket.sendSerialized
 import io.ktor.server.websocket.webSocket
-import io.ktor.websocket.send
 import kotlinx.coroutines.flow.first
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -70,13 +72,23 @@ class KtorWebSocketClientTest {
             port = TEST_SERVER_PORT,
         )
 
-        webSocketClient.sendMessage(pluginId = "pluginId", message = "message")
+        webSocketClient.sendDebuggeeEvent(
+            event = JetWhaleDebuggeeEvent.PluginMessage(
+                pluginId = "pluginId",
+                payload = "message",
+            )
+        )
     }
 
     @Test
-    fun `test receive serverMessage`() = testApplication {
+    fun `test receive debugger event`() = testApplication {
+        val expectedEvent = JetWhaleDebuggerEvent.PluginActivated(
+            pluginId = "testPlugin",
+            pluginVersion = "1.0.0",
+        )
+
         configureTestServer {
-            send(content = "serverMessage")
+            sendSerialized(expectedEvent)
         }
 
         val webSocketClient = KtorWebSocketClient(
@@ -91,8 +103,8 @@ class KtorWebSocketClientTest {
         )
 
         assertEquals(
-            expected = "serverMessage",
-            actual = connectionResult.messageFlow.first()
+            expected = expectedEvent,
+            actual = connectionResult.debuggerEventFlow.first(),
         )
     }
 
