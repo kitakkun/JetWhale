fb77ca5d6b98e8bf3457a992811b7072de49d64e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2026-03-13 00:58:19 +0900

    chore: run spotlessApply

--- jetwhale-host/app/src/main/kotlin/com/kitakkun/jetwhale/host/navigation/JetWhaleNavDisplay.kt
+++ jetwhale-host/app/src/main/kotlin/com/kitakkun/jetwhale/host/navigation/JetWhaleNavDisplay.kt
@@ -39,7 +39,7 @@ fun JetWhaleNavDisplay(
         sceneStrategies = listOf(
             dialogSceneStrategy,
             windowSceneStrategy,
-            listDetailSceneStrategy
+            listDetailSceneStrategy,
         ),
         transitionSpec = {
             ContentTransform(
